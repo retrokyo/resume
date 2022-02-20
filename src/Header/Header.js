cf59f67bb7990c6b0ea8ca38c1952c3aa30de02d
Reeves Froning <froningr@gmail.com>
2022-02-20 19:45:40 +0900

    center image at lower than md

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -6,13 +6,13 @@ import profilePic from "../resume_img_sq.jpeg";
 function Header() {
   return (
     <Row className="gx-0">
-      <Col md="3" className="profilePicDiv px-0">
+      <Col md="3" className="d-flex image-center profilePicDiv px-0">
         <Image roundedCircle src={profilePic} className="profilePic border"></Image>
       </Col>
       <Col md="9" className="title ps-md-4">
-        <h1 className="display-6 text-start">Reeves Froning</h1>
-        <h2 className="text-start">Data Science Engineer </h2>
-        <p className="text-start">
+        <h1 className="display-6 text-start text-center-md">Reeves Froning</h1>
+        <h2 className="text-start text-center-md">Data Science Engineer </h2>
+        <p className="text-start text-center-md">
           I am part software engineer, and part data analyst with a passion for
           learning and applying new technologies
         </p>
