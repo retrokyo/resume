9d596008e1367b171ac0cd6fbc49fd598c6835a1
Reeves Froning <froningr@gmail.com>
2022-05-19 18:06:53 +0900

    fix email address

--- src/Contact/Contact.js
+++ src/Contact/Contact.js
@@ -1,20 +1,23 @@
 function Contact() {
   return (
-      <div className="border-bottom border-light border-3" id="contact-info"> 
-        <h4>Contact Info</h4>
-        <p>
-          <b>Address:</b> 120-0004 Tokyo, Adachi, HigashiAyase
-        </p>
-        <p>
-          <b>Email:</b> froning@gmail.com
-        </p>
-        <p>
-          <b>Phone:</b> 080-4126-2047
-        </p>
-        <p>
-          <b>GitHub:</b> <a target="_blank" href="https://github.com/retrokyo" rel="noreferrer">https://github.com/retrokyo</a>
-        </p>
-      </div>
+    <div className="border-bottom border-light border-3" id="contact-info">
+      <h4>Contact Info</h4>
+      <p>
+        <b>Address:</b> 120-0004 Tokyo, Adachi, HigashiAyase
+      </p>
+      <p>
+        <b>Email:</b> froningr@gmail.com
+      </p>
+      <p>
+        <b>Phone:</b> 080-4126-2047
+      </p>
+      <p>
+        <b>GitHub:</b>{" "}
+        <a target="_blank" href="https://github.com/retrokyo" rel="noreferrer">
+          https://github.com/retrokyo
+        </a>
+      </p>
+    </div>
   );
 }
 
